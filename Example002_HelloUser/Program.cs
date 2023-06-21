f6ad90e13a35215eb592f2786e352af87ece7cf4
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-21 19:42:04 +0300

    Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Доброе время суток ");
+Console.Write(username);
+Console.Write("!");
