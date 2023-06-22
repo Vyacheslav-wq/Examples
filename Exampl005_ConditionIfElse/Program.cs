37d7c94fb67832b1e35faa6071cf8973e1fc4be5
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-22 10:20:01 +0300

    Exampl005_ConditionIfElse

---
+++ Exampl005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿// Приветствие пользователя.
+Console.Clear();
+Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "наташа")
+{
+    Console.Write("Не может быть - это же сама Натали");
+    Console.Write("!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
