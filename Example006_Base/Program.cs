5cb8075638545e8cc7a25a642f9f077098feb842
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-22 12:03:00 +0300

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,19 @@
+﻿// Максимальное число массива.
+int a = 3;
+int b = 58;
+int c = 4;
+int d = 9;
+int e = 12;
+int i = 8;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (i > max) max = i;
+
+Console.Write("max = ");
+Console.Write(max);
