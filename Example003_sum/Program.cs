b3e2c2fe7aaf01a7d66d4455feacae3bfd9b5e6f
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-22 09:38:58 +0300

    Example003_Sum

---
+++ Example003_sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сумма чисел.
+Console.Clear();
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
